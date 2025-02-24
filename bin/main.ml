b9a10e2b6224a39486ff0a98709cdcaa2afb9642
Lucas Du <lucaszdu@gmail.com>
2025-02-24 15:02:39 -0800

    "Fix" command line errors by limiting to printable ASCII
    Still quite a bit to do and decide on.
    1) There are sometimes 'unexpected argument' errors for particular
    runs --- you should try to figure out why this is happening and how to
    avoid it.
    2) Limiting to only printable ASCII is...very limiting. That said, so
    is limiting to non-backtracking regexes so...there are some design
    decisions to think on here.
    
    We can try to just start with this and do some differential testing
    even in this very limited sense between engines that already build in
    non-backtracking as the default. Perhaps you'll still be able to find
    some (shallow) errors.

--- bin/main.ml
+++ bin/main.ml
@@ -11,13 +11,15 @@ open Freak.Ascii
 (* TODO: This is just a messy placeholder to get things to somewhat work. *)
 let run () =
   let parent_dir = Unix.getcwd () in
-  let random_regex = gen_regex 12 in
-  let _ = Rust_regex.setup parent_dir "1.11.1" random_regex in
-  for _ = 1 to 10000 do
-    (* TODO: Set up a timeout here and return failure if match takes too long. *)
-    match Rust_regex.run parent_dir (gen_ascii_string 5) with
-    | WEXITED 0 -> ()
-    | _ -> ()
+  while true do
+    let random_regex = gen_regex 12 in
+    let _ = Rust_regex.setup parent_dir "1.11.1" random_regex in
+    for _ = 1 to 100 do
+      (* TODO: Set up a timeout here and return failure if match takes too long. *)
+      match Rust_regex.run parent_dir (gen_ascii_string 100) with
+      | WEXITED 0 -> ()
+      | _ -> ()
+    done
   done
 
 let _compare_out = ()
--- lib/ascii.ml
+++ lib/ascii.ml
@@ -1,6 +1,7 @@
 (** [gen_ascii_string] creates a string containing [size] random bytes. Note
     that a byte is represented by the [char] type in the [Bytes] module. *)
 let gen_ascii_string size =
-  Bytes.to_string (Bytes.init size (fun _ -> Random.int 256 |> Char.chr))
+  String.of_bytes (Bytes.init size
+                     (fun _ -> Random.int_in_range ~min:32 ~max:126 |> Char.chr))
 
 let gen_ascii_option_string maxchars =  gen_ascii_string (Random.int maxchars)
