2d0f7b8c9b6d2fe25329ff8bf565052aeb3e3550
Lucas Du <lucaszdu@gmail.com>
2025-03-05 16:39:03 -0800

    Save latest updates to mismatch checker
    Save mismatching output to a specifically named directories. Had to a
    good bit of Unix munging, but it roughly works now.

--- bin/main.ml
+++ bin/main.ml
@@ -9,19 +9,9 @@ open Printf
 let gen_ascii_input_string size =
   String.init size (fun _ -> gen_ascii_char ())
 
-let compare_out input wrapper_dir =
-  (* TODO: Should consider how this will interact with logging --- if logs are
-     turned on, should save all inputs and related output to a file. Otherwise,
-     only save mismatches (as you do already here). *)
-  let rust_regex_out = Rust_regex.run_wrap wrapper_dir input in
-  let go_regexp_out = Go_regexp.run_wrap wrapper_dir input in
-  if not (String.equal rust_regex_out go_regexp_out) then
-    print_endline (sprintf "mismatch (rust-regex=%s, go-regexp=%s) on input %s"
-                     rust_regex_out go_regexp_out input)
-
 let run () =
-  let parent_dir = Unix.getcwd () in
-  let wrapper_dir = sprintf "%s/_freak_wrappers" parent_dir in
+  let cwd = Unix.getcwd () in
+  let wrapper_dir = sprintf "%s/_freak_wrappers" cwd in
   (* Clean up _freak_wrappers first if it exists *)
   if Sys.file_exists(wrapper_dir) then
     let _ = Unix.system (Filename.quote_command "rm" ["-rf"; wrapper_dir]) in ();
@@ -30,18 +20,52 @@ let run () =
   Unix.mkdir wrapper_dir 0o777;
   let _ = Go_regexp.setup_env wrapper_dir "" in
   let _ = Rust_regex.setup_env wrapper_dir "1.11.1" in
+  let mismatch_dir = sprintf "%s/_mismatches_found" cwd in
+  Unix.mkdir mismatch_dir 0o777;
   while true do
-    let random_regex = gen_regex 8 in
-    print_endline (sprintf "TEST REGEX: %s" (Go_regexp.realize_regex random_regex));
+    let random_regex = gen_regex 5 in
+    let realized_regex = realize_re2_regex random_regex in
     let _  = Go_regexp.pre_wrap wrapper_dir random_regex in
     let _  = Rust_regex.pre_wrap wrapper_dir random_regex in
     for _ = 1 to 100 do
-      (* TODO: Set up a timeout here. *)
       let input = (gen_ascii_input_string 64) in
       (* TODO: Read results from stdout file descriptor, do live multithreaded
          comparison for each input. Save full inputs and wrappers for each
          discrepancy. *)
-      compare_out input wrapper_dir
+      let rust_regex_out = Rust_regex.run_wrap wrapper_dir input in
+      let go_regexp_out = Go_regexp.run_wrap wrapper_dir input in
+      if not (String.equal rust_regex_out go_regexp_out) then
+        begin
+          (* If there is a mismatch, save input and regex engine outputs,
+             along with corresponding wrapper files, into a directory. The
+             name of the directory is a base64 encoding of the realized regex
+             string (to try and maintain uniqueness). *)
+          let regex_mismatch_dir =
+            let b64enc_regex = Filename.quote (Base64.encode_exn realized_regex) in
+            sprintf "%s/%s" mismatch_dir b64enc_regex in
+          if not (Sys.file_exists regex_mismatch_dir) then
+            begin
+              Unix.mkdir regex_mismatch_dir 0o777;
+              let rust_regex_file =
+                Rust_regex.wrapper_file_full wrapper_dir in
+              let _ =
+                Unix.system(Filename.quote_command
+                              "cp" [rust_regex_file; regex_mismatch_dir]) in
+              let go_regexp_file =
+                Go_regexp.wrapper_file_full wrapper_dir in
+              let _ =
+                Unix.system(Filename.quote_command
+                              "cp" [go_regexp_file; regex_mismatch_dir]) in ()
+            end;
+          (* Write mismatch output to a file. *)
+          let mismatch_file = sprintf "%s/mismatches.log" regex_mismatch_dir in
+          let b_mismatch_out =
+            sprintf "mismatch on input %s\n==> rust-regex: %s\n==> go-regexp: %s\n"
+              input rust_regex_out go_regexp_out |> Bytes.of_string in
+          let fd = Unix.openfile mismatch_file [O_CREAT; O_WRONLY; O_APPEND] 0o764 in
+          let _ = Unix.write fd b_mismatch_out 0 (Bytes.length b_mismatch_out) in
+          Unix.close fd
+        end
     done
   done
 
--- lib/wrappers.ml
+++ lib/wrappers.ml
@@ -18,75 +18,98 @@ let check_exists (exe : string) : unit =
   | WSIGNALED s -> raise (Setup_error (sprintf "%s killed by signal %d." exe s))
   | WSTOPPED s -> raise (Setup_error (sprintf "%s killed by signal %d." exe s))
 
-(* TODO: Instead of returning a raw Unix.process_status, try to return some
-   option type defined in this module. *)
+let gen_ascii_string size =
+  let ascii_string_escaped () =
+    let c = gen_ascii_char () in
+    match c with
+    | '\\' -> {|\\|}
+    | '{' -> {|\{|}
+    | '}' -> {|\}|}
+    | '[' -> {|\[|}
+    | ']' -> {|\]|}
+    | '(' -> {|\(|}
+    | ')' -> {|\)|}
+    | '+' -> {|\+|}
+    | '$' -> {|\$|}
+    | '^' -> {|\^|}
+    | '\"' -> {|\\"|}
+    | _ -> (Bytes.make 1 c) |> Bytes.to_string
+  in
+  let rec aux str s =
+    if s = 0 then str else
+      aux ((ascii_string_escaped ()) ^ str) (s - 1)
+  in
+  aux "" size
+
+let rec realize_re2_regex (r: regex) : string =
+  let realize_cs = function
+    | Char -> gen_ascii_string ((Random.int 5) + 1)
+    (* TODO: Should figure out how to handle this "empty" case; also how to
+        generally get random whitespace and newlines/returns. *)
+    | Empty -> " "
+    | Any -> "."
+    | Digit -> "0-9"
+    | AnyLetter -> "a-zA-Z"
+    | CapLetter -> "A-Z"
+    | LowLetter -> "a-z"
+  in
+  match r with
+  | CharSet cs -> realize_cs cs
+  | Not cs -> "[^" ^ realize_cs cs ^ "]"
+  | And (cs1, cs2) -> realize_cs cs1 ^ "&&" ^ realize_cs cs2
+  | StartsWith p -> "^" ^ realize_re2_regex p
+  | EndsWith p -> realize_re2_regex p ^ "$"
+  | Concat (p1, p2) -> realize_re2_regex p1 ^ realize_re2_regex p2
+  | Or (p1, p2) -> realize_re2_regex p1 ^ "|" ^ realize_re2_regex p2
+  (* NOTE: Add parentheses around all repetition operators as a semi-hack that
+      prevents syntactic errors on nested repetitions (at the cost of adding
+      capture group semantics). *)
+  | Optional p -> "(" ^ realize_re2_regex p ^ ")" ^ "?"
+  | KleeneStar p -> "(" ^ realize_re2_regex p ^ ")" ^ "*"
+  | Repeat (p, n) -> "(" ^ realize_re2_regex p ^ ")" ^ (sprintf "{%d}" n)
+  | RepeatAtLeast (p, n) -> "(" ^ realize_re2_regex p ^ ")" ^ (sprintf "{%d,}" n)
+  | RepeatRange (p, startn, endn) ->
+      "(" ^ realize_re2_regex p ^ ")" ^ (sprintf "{%d,%d}" startn endn)
+
+let construct_path parent child = sprintf "%s/%s" parent child
+
+let run_wrapper_exe exe input =
+  let cmd = Filename.quote_command exe [input] in
+  (* TODO: Using this concurrent operation is causing some confusion in the
+      stderr output where things are getting mixed together. Try to find a way
+      around this. *)
+  let inp = Unix.open_process_in cmd in
+  let r = In_channel.input_line inp in
+  In_channel.close inp;
+  match r with
+  | Some out -> out
+  | None -> "error"
+
 module type WRAPPER = sig
+  val compiler : string
+  val wrapper_name : string
+  val wrapper_file_full : string -> string
+  val wrapper_exec_full : string -> string
   val realize_regex : regex -> string
   val produce_program : regex -> string
+  (* TODO: Instead of returning a raw Unix.process_status, try to return some
+     option type defined in this module. *)
   val setup_env : string -> string -> Unix.process_status
   val pre_wrap : string -> regex -> Unix.process_status
-  val run_wrap : string-> string -> string
+  val run_wrap : string -> string -> string
 end
 
-(* TODO P1: Need to fix Rust regex generation to stop failing various syntactic
-   checks. *)
 module Rust_regex : WRAPPER = struct
   let compiler = "rustc"
-  let project_dir = "rust-regex-wrap"
+  let wrapper_name = "rust-regex-wrap"
+  (* NOTE: src/main.rs is the cargo convention for program entry point *)
+  let wrapper_file_full parent_dir =
+    sprintf "%s/%s/%s" parent_dir wrapper_name "src/main.rs"
+  (* NOTE: cargo builds executables in target/debug *)
+  let wrapper_exec_full parent_dir =
+    sprintf "%s/%s/target/debug/%s" parent_dir wrapper_name wrapper_name
 
-  let gen_ascii_string size =
-    let ascii_string_escaped () =
-      let c = gen_ascii_char () in
-      (* TODO: There is probably more to do here. *)
-      match c with
-      | '\\' -> {|\\|}
-      | '{' -> {|\{|}
-      | '}' -> {|\}|}
-      | '[' -> {|\[|}
-      | ']' -> {|\]|}
-      | '(' -> {|\(|}
-      | ')' -> {|\)|}
-      | '+' -> {|\+|}
-      | '$' -> {|\$|}
-      | '^' -> {|\^|}
-      | '\"' -> {|\\"|}
-      | _ -> (Bytes.make 1 c) |> Bytes.to_string
-    in
-    let rec aux str s =
-      if s = 0 then str else
-        aux ((ascii_string_escaped ()) ^ str) (s - 1)
-    in
-    aux "" size
-
-  let rec realize_regex (r: regex) : string =
-    let realize_cs = function
-      | Char -> gen_ascii_string ((Random.int 5) + 1)
-      (* TODO: Should figure out how to handle this "empty" case; also how to
-         generally get random whitespace and newlines/returns. *)
-      | Empty -> " "
-      | Any -> "."
-      | Digit -> "0-9"
-      | AnyLetter -> "a-zA-Z"
-      | CapLetter -> "A-Z"
-      | LowLetter -> "a-z"
-    in
-    match r with
-    | CharSet cs -> realize_cs cs
-    | Not cs -> "[^" ^ realize_cs cs ^ "]"
-    | And (cs1, cs2) -> realize_cs cs1 ^ "&&" ^ realize_cs cs2
-    | StartsWith p -> "^" ^ realize_regex p
-    | EndsWith p -> realize_regex p ^ "$"
-    | Concat (p1, p2) -> realize_regex p1 ^ realize_regex p2
-    | Or (p1, p2) -> realize_regex p1 ^ "|" ^ realize_regex p2
-    (* NOTE: Add parentheses around all repetition operators as a semi-hack that
-       prevents syntactic errors on nested repetitions (at the cost of adding
-       capture group semantics). *)
-    | Optional p -> "(" ^ realize_regex p ^ ")" ^ "?"
-    | KleeneStar p -> "(" ^ realize_regex p ^ ")" ^ "*"
-    | Repeat (p, n) -> "(" ^ realize_regex p ^ ")" ^ (sprintf "{%d}" n)
-    | RepeatAtLeast (p, n) -> "(" ^ realize_regex p ^ ")" ^ (sprintf "{%d,}" n)
-    | RepeatRange (p, startn, endn) ->
-       "(" ^ realize_regex p ^ ")" ^ (sprintf "{%d,%d}" startn endn)
+  let realize_regex = realize_re2_regex
 
   let produce_program r =
     let realr = realize_regex r in
@@ -113,109 +136,52 @@ fn main() {
     }
 }" realr
 
-  (* [setup_env] contains all the side-effecting imperative code needed to get
-     the Rust build environment --- i.e. the Rust compiler and the Cargo build
-     tool --- up and ready. *)
   let setup_env parent_dir engine_version =
     (* Ensure compiler exists *)
     check_exists(compiler);
     (* Ensure cargo is installed *)
     check_exists("cargo");
     (* Create project directory *)
-    let full_project_dir = sprintf "%s/%s" parent_dir project_dir in
+    let wrapper_dir = construct_path parent_dir wrapper_name in
     (* TODO: Try to set up permissions (and general structure!) for these files
        in a more principled way. *)
-    if (not (Sys.file_exists full_project_dir)) then
-      Unix.mkdir full_project_dir 0o777;
+    if (not (Sys.file_exists wrapper_dir)) then
+      Unix.mkdir wrapper_dir 0o777;
     (* Set up basic cargo project*)
-    let _ = Unix.system(Filename.quote_command "cargo" ["init"; full_project_dir]) in
+    let _ = Unix.system(Filename.quote_command "cargo" ["init"; wrapper_dir]) in
     (* Ensure required crates are installed *)
-    Unix.chdir full_project_dir;
+    Unix.chdir wrapper_dir;
     let _ = Unix.system(Filename.quote_command "cargo" ["add"; "once_cell"]) in
     let regex_crate_name = sprintf "regex@%s" engine_version in
     Unix.system(Filename.quote_command "cargo" ["add"; regex_crate_name])
 
   let pre_wrap parent_dir r =
-    let full_project_dir = sprintf "%s/%s" parent_dir project_dir in
+    let wrapper_dir = construct_path parent_dir wrapper_name in
     (* Write wrapper program to file *)
     let content = produce_program r in
-    (* NOTE: src/main.rs is the cargo convention for entry point *)
-    let filename = sprintf "%s/src/main.rs" full_project_dir in
+    let filename = wrapper_file_full parent_dir in
     (* NOTE: The file permissions must be in octal. *)
     let fd = Unix.openfile filename [O_CREAT; O_WRONLY; O_TRUNC] 0o764 in
     let bcontent = Bytes.of_string content in
     let _ = Unix.write fd bcontent 0 (Bytes.length bcontent) in
     Unix.close fd;
-    Unix.chdir full_project_dir;
+    Unix.chdir wrapper_dir;
     let build_cmd = Filename.quote_command "cargo" ["build"] in
     Unix.system(build_cmd)
 
   let run_wrap parent_dir input =
-    let full_project_dir = sprintf "%s/%s" parent_dir project_dir in
-    (* NOTE: cargo builds executables in target/debug *)
-    let executable = sprintf "%s/target/debug/%s" full_project_dir project_dir in
-    let cmd = Filename.quote_command executable [input] in
-    let inp = Unix.open_process_in cmd in
-    let r = In_channel.input_all inp in
-    In_channel.close inp; r
+    run_wrapper_exe (wrapper_exec_full parent_dir) input
 end
 
 module Go_regexp : WRAPPER = struct
   let compiler = "go"
-  let project_dir = "go-regexp-wrap"
+  let wrapper_name = "go-regexp-wrap"
+  let wrapper_file_full parent_dir =
+    sprintf "%s/%s/%s" parent_dir wrapper_name "main.go"
+  let wrapper_exec_full parent_dir =
+    sprintf "%s/%s/%s" parent_dir wrapper_name wrapper_name
 
-  let gen_ascii_string size =
-    let ascii_string_escaped () =
-      let c = gen_ascii_char () in
-      match c with
-      | '\\' -> {|\\|}
-      | '{' -> {|\{|}
-      | '}' -> {|\}|}
-      | '[' -> {|\[|}
-      | ']' -> {|\]|}
-      | '(' -> {|\(|}
-      | ')' -> {|\)|}
-      | '+' -> {|\+|}
-      | '$' -> {|\$|}
-      | '^' -> {|\^|}
-      | '\"' -> {|\\"|}
-      | _ -> (Bytes.make 1 c) |> Bytes.to_string
-    in
-    let rec aux str s =
-      if s = 0 then str else
-        aux ((ascii_string_escaped ()) ^ str) (s - 1)
-    in
-    aux "" size
-
-  let rec realize_regex (r: regex) : string =
-    let realize_cs = function
-      | Char -> gen_ascii_string ((Random.int 5) + 1)
-      (* TODO: Should figure out how to handle this "empty" case; also how to
-         generally get random whitespace and newlines/returns. *)
-      | Empty -> " "
-      | Any -> "."
-      | Digit -> "0-9"
-      | AnyLetter -> "a-zA-Z"
-      | CapLetter -> "A-Z"
-      | LowLetter -> "a-z"
-    in
-    match r with
-    | CharSet cs -> realize_cs cs
-    | Not cs -> "[^" ^ realize_cs cs ^ "]"
-    | And (cs1, cs2) -> realize_cs cs1 ^ "&&" ^ realize_cs cs2
-    | StartsWith p -> "^" ^ realize_regex p
-    | EndsWith p -> realize_regex p ^ "$"
-    | Concat (p1, p2) -> realize_regex p1 ^ realize_regex p2
-    | Or (p1, p2) -> realize_regex p1 ^ "|" ^ realize_regex p2
-    (* NOTE: Add parentheses around all repetition operators as a semi-hack that
-       prevents syntactic errors on nested repetitions (at the cost of adding
-       capture group semantics). *)
-    | Optional p -> "(" ^ realize_regex p ^ ")" ^ "?"
-    | KleeneStar p -> "(" ^ realize_regex p ^ ")" ^ "*"
-    | Repeat (p, n) -> "(" ^ realize_regex p ^ ")" ^ (sprintf "{%d}" n)
-    | RepeatAtLeast (p, n) -> "(" ^ realize_regex p ^ ")" ^ (sprintf "{%d,}" n)
-    | RepeatRange (p, startn, endn) ->
-       "(" ^ realize_regex p ^ ")" ^ (sprintf "{%d,%d}" startn endn)
+  let realize_regex = realize_re2_regex
 
   let produce_program r =
     let realr = realize_regex r in
@@ -242,14 +208,14 @@ func main() {
     (* Ensure compiler exists *)
     check_exists(compiler);
     (* Create project directory *)
-    let full_project_dir = sprintf "%s/%s" parent_dir project_dir in
+    let wrapper_dir = construct_path parent_dir wrapper_name in
     (* TODO: Try to set up permissions (and general structure!) for these files
        in a more principled way. *)
-    if (not (Sys.file_exists full_project_dir)) then
-      Unix.mkdir full_project_dir 0o777;
-    Unix.chdir full_project_dir;
+    if (not (Sys.file_exists wrapper_dir)) then
+      Unix.mkdir wrapper_dir 0o777;
+    Unix.chdir wrapper_dir;
     let init_status = Unix.system(Filename.quote_command "go"
-                                    ["mod"; "init"; project_dir]) in
+                                    ["mod"; "init"; wrapper_name]) in
     (* Ensure specified regexp version is installed *)
     (* NOTE: This check seems fairly brittle. *)
     if String.length(engine_version) <> 0 then
@@ -259,26 +225,19 @@ func main() {
       init_status
 
   let pre_wrap parent_dir r =
-    let full_project_dir = sprintf "%s/%s" parent_dir project_dir in
+    let wrapper_dir = construct_path parent_dir wrapper_name in
     (* Write wrapper program to file *)
     let content = produce_program r in
-    let filename = sprintf "%s/main.go" full_project_dir in
+    let filename = wrapper_file_full parent_dir in
     (* NOTE: The file permissions must be in octal. *)
     let fd = Unix.openfile filename [O_CREAT; O_WRONLY; O_TRUNC] 0o764 in
     let bcontent = Bytes.of_string content in
     let _ = Unix.write fd bcontent 0 (Bytes.length bcontent) in
     Unix.close fd;
-    Unix.chdir full_project_dir;
+    Unix.chdir wrapper_dir;
     let build_cmd = Filename.quote_command "go" ["build"] in
     Unix.system(build_cmd)
 
   let run_wrap parent_dir input =
-    let executable = sprintf "%s/%s/%s" parent_dir project_dir project_dir in
-    let cmd = Filename.quote_command executable [input] in
-    (* TODO: Using this concurrent operation is causing some confusion in the
-       stderr output where things are getting mixed together. Try to find a way
-       around this. *)
-    let inp = Unix.open_process_in cmd in
-    let r = In_channel.input_all inp in
-    In_channel.close inp; r
+    run_wrapper_exe (wrapper_exec_full parent_dir) input
 end
